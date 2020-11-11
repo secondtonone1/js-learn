9cfc6541dc86470f2b43a92919e4bbf436d9c68c
secondtonone1 <secondtonone1@163.com>
2020-11-11 16:06:54 +0800

    delete

--- module/main.js
+++
@@ -1,4 +0,0 @@
-'use strict '
-var greet = require('./hello')
-var Michal = 'Michal'
-greet(Michal)
--- stream/pipewrite.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-
-var rs = fs.createReadStream('sample.txt');
-var ws = fs.createWriteStream('copied.txt');
-
-rs.pipe(ws);
